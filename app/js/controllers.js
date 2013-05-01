6aa7739d079990cd4a17e47f2cc6f123134469c8
Marten <martenc@gmail.com>
2013-05-01 14:33:23 -0700

    Initial add including dynamic content to be iframed into the bio modal

---
+++ app/js/controllers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Controllers */
+
+function HomeCtrl($scope, Character) {
+	$scope.characters = Character.query();
+  	$scope.orderProp = 'age';
+}
+
+
+function BioCtrl($scope, $routeParams, Character) {
+  $scope.character = Character.get({characterName: $routeParams.characterName}, function(character) {
+  });
+
+  // the ng preferred way of setting images in views/partials
+  $scope.setImage = function(bio_image_big) {
+    $scope.bio_image_big = bio_image_big;
+  }
+  
+}
+
